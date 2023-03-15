8b7a5b9ffd12155236718a05f699b9f1b0a2274d
antonrejoe <antonrejoe@gmail.com>
2023-03-15 08:58:41 +0530

    still %.2f

---
+++ index.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+// int main()
+// {
+
+//     int num;
+//     num = 10;
+//     printf("%d", sizeof(num));
+// }
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    float x, p;
+    double y, q;
+    unsigned k;
+
+    int m = 323232;
+    long int n = 23122;
+
+    x = 1.0;
+    y = 0.23232;
+    k = 0;
+    p = q = 0.0;
+
+    double angle = 15.55;
+
+    printf("Angle = %.2f degrees \n", angle);
+}
