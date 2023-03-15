0f82c4ac4bc7cc2369a5adf375cb224dcc432e9c
antonrejoe <antonrejoe@gmail.com>
2023-03-15 09:33:15 +0530

    1st

--- index.c
+++ index.c
@@ -12,9 +12,12 @@
 int main(int argc, char const *argv[])
 {
     /* code */
-    int sum = 65;
-    double average = 12.368;
-    char ch = 'b';
+    printf("%d , %d , %d\n", 44, 55);     // output 44  , 55 , -19744977064
+    printf("%d , %d , %d\n", 44, 55, 66); // output 44 , 55 , 66
 
-    printf("sum = %5i, average = %.7f \n", sum, average);
+    // have a look at the  comma guy in b.w
+
+    printf("%d %d %d\n", 44, 55);     // output 44 55 0
+    printf("%d %d %d\n", 44, 55, 66); // output 44 55 66
+    printf("%d %d\n", 44, 55, 66);    // output 44 55  #ignoreS
 }
