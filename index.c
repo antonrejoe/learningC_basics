3096464506128df2e536d398c5c86b8e10115c3e
antonrejoe <antonrejoe@gmail.com>
2023-03-17 11:47:01 +0530

    format width specifier

--- index.c
+++ index.c
@@ -11,13 +11,8 @@
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    printf("%d , %d , %d\n", 44, 55);     // output 44  , 55 , -19744977064
-    printf("%d , %d , %d\n", 44, 55, 66); // output 44 , 55 , 66
+    char str[] = "hello   \n world";
 
-    // have a look at the  comma guy in b.w
-
-    printf("%d %d %d\n", 44, 55);     // output 44 55 0
-    printf("%d %d %d\n", 44, 55, 66); // output 44 55 66
-    printf("%d %d\n", 44, 55, 66);    // output 44 55  #ignoreS
+    printf("%d\n", sizeof(str));
+    printf("%s", str);
 }
