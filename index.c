8783007a6f08349003dd1c385234f23783adff37
antonrejoe <antonrejoe@gmail.com>
2023-03-17 12:24:16 +0530

    relational operators

--- index.c
+++ index.c
@@ -12,9 +12,16 @@
 int main(int argc, char const *argv[])
 {
     // relational operators
-    bool result;
-    int num1 = 5, num2 = 6;
-    result = convertBoolean((num1 < num2));
-    printf("Is %d < %d: %d\n", num1, num2, (num1 < num2)); // Is 5 < 6: 1
-    printf("Is %d < %d: %d\n", num1, num2, result);        // Is 5 < 6: 1
-}
+    int num1=6 ,num2=5 ,num3=-1;
+
+    int lsThan = num1 <num2;
+    int lsThanEq = num1<= num2;
+    int greThan = num1 > num2;
+    printf("Original numbers : %d %d\n",num1 , num2);
+    printf("Outputs of relational expressions:%d,%d,%d\n",lsThan,lsThanEq,greThan); //  0 , 0 , 1
+    printf("%d && %d hello  %d\n", (num1>num2) , ((num1 >= num2 && (num2 !=num1)) )); 2 &&
+    printf("%d : %d\n", !(num1 > num2), (num1  > num2)); //  0 : 1
+    printf("Output is %d\n" , (num1 && num3)); // Output is 1
+    printf("Current value %d\n" , num1++); // Current value 6
+    printf("Now the value of num1 is %d\n" , num1); // Now the value of num1 is 7
+    }
