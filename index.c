187916af8e089ee08a8191c2a2fb2bd8ac350ea1
antonrejoe <antonrejoe@gmail.com>
2023-03-17 12:06:12 +0530

    final change

--- index.c
+++ index.c
@@ -11,17 +11,16 @@
 
 int main(int argc, char const *argv[])
 {
+    float c, d;
 
-    int a, b, c;
-    printf("%s", "3434df");
-    scanf("%d%*d%d\n", &a, &b, &c);
-    // %*d is ignored value => ie. &b is skipped but the next value is given to it and &c is ignored
-    // otuput value :
-    // 3434df
-    // 2
-    // 3
-    // 1
-    // 3
-    // 2 1 0
-    printf("%d %d %d\n", a, b, c);
+    c = 5 / 9;
+    d = 5.0 / 9;
+
+        printf("%f\n", c); // 0.000000
+    printf("%d\n", c); // 198898336
+    printf("%f\n", d); // 0.555556
+    printf("%d\n", d); // 198898336
+
+    // for floating operation we need to have atleast one floating point number
+    // if 2 integers are present then integer operation is carried out
 }
