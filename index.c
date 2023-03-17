1d6a7681a21767a1c14746c4b6e64513051842d3
antonrejoe <antonrejoe@gmail.com>
2023-03-17 11:57:29 +0530

    %*d

--- index.c
+++ index.c
@@ -11,8 +11,17 @@
 
 int main(int argc, char const *argv[])
 {
-    char str[] = "hello   \n world";
 
-    printf("%d\n", sizeof(str));
-    printf("%s", str);
+    int a, b, c;
+    printf("%s", "3434df");
+    scanf("%d%*d%d\n", &a, &b, &c);
+    // %*d is ignored value => ie. &b is skipped but the next value is given to it and &c is ignored
+    // otuput value :
+    // 3434df
+    // 2
+    // 3
+    // 1
+    // 3
+    // 2 1 0
+    printf("%d %d %d\n", a, b, c);
 }
