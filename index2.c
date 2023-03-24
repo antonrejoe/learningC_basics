a012036acf7e16652a5d653d773fcd1d4af46f30
antonrejoe <antonrejoe@gmail.com>
2023-03-24 11:49:26 +0530

    " some conditionals"

--- index2.c
+++ index2.c
@@ -6,8 +6,14 @@
 int main(){
 
     int value = 1 , result;
+    int i = (5,10);  // i = 5
+    int j = (i ? 7:10); // checks whether i = 7 if not then it's assigned the value of 10
+
     result = (value++ *5) + (++value*3);
-    printf("%f", result);
-    printf(("%f", value));
+    printf("%d\n", result);
+    printf("%d\n", value);
+    printf("%d\n", i); // the second value will be given out
+    printf("%d\n", j); // ? => if condition : else condn
+
     return 0;
 }
